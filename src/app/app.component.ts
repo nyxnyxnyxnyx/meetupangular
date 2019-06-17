48de264cd0b494b9cd6f08a3f0f0b74932aacf12
Alexander Vilchez <a.vilchezr@pucp.pe>
2019-06-16 19:53:06 -0500

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,44 @@ import { ChatService } from './chat.service';
 })
 export class AppComponent {
   msgInput : string;
+  loginInput : string;
+  isLogin : boolean;
   msgs = [];
+  writting: boolean;
 
   constructor(private chatService : ChatService) {}
 
   ngOnInit() {
     this.chatService
-        .getMessage()
-        .subscribe(msg => {
-          this.msgs.push({type:2,msg:msg})
-          if(this.msgs.length>7){
-            setTimeout(()=>this.msgs.pop(),5000)
-            
-          }
-        });
+    .getMessage()
+    .subscribe(msg => {
+      this.msgs.push({type:2,msg:msg})
+      if(this.msgs.length>7){
+        setTimeout(()=>this.msgs.pop(),3000)
+      }
+    });
+
+    this.chatService
+    .getWritting()
+    .subscribe(state => {
+      this.writting=state
+
+    });
   }
 
   sendMsg(){
     if(this.msgInput!=""){
       this.msgs.push({type:1,msg:this.msgInput})
-      this.chatService.sendMessage(this.msgInput);
+      this.chatService.sendMessage('message',this.msgInput);
       this.msgInput="";
+    }
+  }
+
+  login(){
+    if(this.msgInput!=""){
+      this.chatService.sendMessage('login',this.loginInput);
+      this.msgs.push({type:2,msg:"Bienvenido a ZapWhat, "+this.loginInput})
+      this.isLogin=true;
     }
   }
 }
